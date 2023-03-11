00104ef7e3b2458d23ce702640e86c09e43cb3f6
Mahmoud Fouad <mfapolice@gmail.com>
2023-03-11 20:56:10 +0200

    update

--- solveProblems.js
+++ solveProblems.js
@@ -17,7 +17,51 @@
 //////////////////////////////////////////////
 // First non-repeating character
 function firstNonRepeatingLetter(string) {
-return string.split("").map((s,i,arr) => s === arr[i++]);
+  const arr = string.split('');
+  
+  for (let i = 0; i < arr.length; i++) {
+    const chr = arr[i];
+    // console.log(chr);
+    // console.log('arr.indexOf(arr[i]) :>> ', arr.indexOf(arr[i]));
+    console.log('arr.lastIndexOf(arr[i]) :>> ', arr.lastIndexOf(arr[i]));
+    if (arr.indexOf(arr[i]) === arr.lastIndexOf(arr[i])) {
+      return arr[i]
+    }
+    
+  }
 }
 
+// function firstNotRepeatingCharacter(s) {
+        
+//   const arr = s.split("");
+
+//   for(let i = 0; i < arr.length; i++){
+//       let chr = arr[i];
+//       if( arr.indexOf(arr[i]) == arr.lastIndexOf(arr[i])){
+//           return arr[i]
+//       }            
+//   }
+  
+//   // return "_"
+// }
+
 console.log(firstNonRepeatingLetter("stress"));
+// console.log(firstNotRepeatingCharacter("stress"));
+
+// const arr = ["d", "d", "e", "e", "e", "k", "j", "j", "h"];
+// const firstNonRepeating = (arr) => {
+//   let count = 0;
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     if (arr[i] !== arr[i + 1]) {
+//       console.log("arr[i]", arr[i], "arr[i + 1]", arr[i + 1]);
+//       if (!count) {
+//         return i;
+//       }
+//       count = 0;
+//     } else {
+//       count++;
+//     }
+//   }
+//   return -1;
+// };
+// console.log(firstNonRepeating(arr));
