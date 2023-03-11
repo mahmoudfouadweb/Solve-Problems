433c81330be6833b444c5d1700e7314f61114452
Mahmoud Fouad <mfapolice@gmail.com>
2023-03-11 07:03:58 +0200

    update

--- solveProblems.js
+++ solveProblems.js
@@ -1,15 +1,23 @@
-const toWeirdCase = (string) =>
-  string
-    .trim()
-    .split(" ")
-    .map((s) =>
-      s
-        .split("")
-        .map((l, idx) => (idx % 2 === 0 ? l.toUpperCase() : l.toLowerCase()))
-        .join("")
-    )
-    .join(" ");
+// const toWeirdCase = (string) =>
+//   string
+//     .trim()
+//     .split(" ")
+//     .map((s) =>
+//       s
+//         .split("")
+//         .map((l, idx) => (idx % 2 === 0 ? l.toUpperCase() : l.toLowerCase()))
+//         .join("")
+//     )
+//     .join(" ");
 
-toWeirdCase("this man");
-console.log(toWeirdCase("this man"));
-console.log(toWeirdCase("this "));
+// toWeirdCase("this man");
+// console.log(toWeirdCase("this man"));
+// console.log(toWeirdCase("this "));
+
+//////////////////////////////////////////////
+// First non-repeating character
+function firstNonRepeatingLetter(s) {
+  // Add your code here
+}
+
+firstNonRepeatingLetter('stress')
